113c57583981e496551e76d4210bf1f59d792bc9
kenneth koontz <kenneth.koontz@gmail.com>
2012-03-24 16:49:40 -0700

    Removed default index.html and created home view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Litmus::Application.routes.draw do
+  get "home/index"
+
   resources :test_cases
 
   resources :test_suites do
@@ -58,7 +60,7 @@ Litmus::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
