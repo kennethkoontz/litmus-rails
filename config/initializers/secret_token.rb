da8fee227a3e8128db17057c877b25f56c3c081e
kenneth koontz <kenneth.koontz@gmail.com>
2012-03-24 14:33:40 -0700

    Created Litmus Test Case Manager project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Litmus::Application.config.secret_token = '42b56ecd5c03d4ae915be96f3be0050d8df46ef6511a4cfdd6b2270ea00b58862471b562b6c43fbc66c9f527a3135fbc01549201d19f949821ef55c628ccc66b'
