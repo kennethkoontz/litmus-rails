d1bbab6ceeff118e30d85faab4df5a11083b34a5
kenneth koontz <kenneth.koontz@gmail.com>
2012-03-29 14:47:09 -0700

    Updated development environment to work with mysql

--- Gemfile
+++ Gemfile
@@ -39,3 +39,4 @@ gem 'jquery-rails'
 
 gem "twitter-bootstrap-rails"
 gem "thinking-sphinx"
+gem "mysql2"
--- app/models/test_case.rb
+++ app/models/test_case.rb
@@ -1,3 +1,12 @@
 class TestCase < ActiveRecord::Base
   belongs_to :test_suite
+
+  define_index do
+    indexes title,
+    indexes setup
+    indexes test_plan
+    indexes expected_results
+    indexes test_suite.title, :as => test_suite_title
+  end
+
 end
